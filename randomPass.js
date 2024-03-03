a0e398c141cb2d39272bbf24b9ac2a235126a5b9
Piraveen17 <piraveen1727@gmail.com>
2024-03-03 18:57:46 +0530

    javascript file created

---
+++ randomPass.js
@@ -0,0 +1,17 @@
+const passwordBox = document.getElementById("password");
+const length = document.getElementById("length-number");
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "!@#$%^&*()?><";
+const allChars = upperCase + lowerCase + number + symbol;
+
+function generate() {
+  let password = "";
+  console.log(length);
+  while (password.length < length.value) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+  passwordBox.value = password;
+}
